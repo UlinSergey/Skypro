0fcce078396da784d32439c2246210b5d8d2ea2a
Sergey Ulin <PYLIN2002@yandex.ru>
2023-11-29 16:09:14 +0300

    домашнее задание логин пароль

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,45 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Задание №1");
-        for (int i = 1; i <= 10; i = i + 1) {
-            System.out.println(i);
-        }
 
-        System.out.println("Задание №2");
-        for (int i = 10; i > 0; i = i - 1) {
-            System.out.println(i);
+    private static final String VALIDATE_PATTERN="^[a-zA-Z0-9-_]+$";
+    public static void main(String[] args) {
+        check("login", "pass","pass");
+        check("login", "pass","pass2");
+        check("login", "passpasspasspasspasspasspasspasspasspasspasspasspasspasspasspass","pass");
+        check("loginloginloginloginloginloginloginloginloginlogin", "pass","pass");
+        check("login", "pass%%%%","pass");
+        check("login%%%%%", "pass","pass");
+
+    }
+
+    private static void check(String login, String pass, String confirmPas){
+        try {
+            checkLogin(login);
+            checkPassword(pass, confirmPas);
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка с введенным логином: " + e.getMessage());
+        } catch (WrongPasswordException e){
+            System.out.println("Ошибка с введенным паролем: "+ e.getMessage());
         }
-        System.out.println("Задание №3");
-        for (int i = 0; i <= 17; i = i + 2) {
-            //Нуль не может быть как четной так и нечетной поэтому будь бы чуть другие условия использовал бы:
-            //for (int i = 2; i <= 17; i = i + 2) {
-            System.out.println(i);
+    }
+
+    private static void checkLogin (String login) throws WrongLoginException{
+        if (!login.matches(VALIDATE_PATTERN)){
+            throw new WrongLoginException("Логин может содержакть в себе только латинские буквы, цыфры и знаки подчеркивания");
         }
-        System.out.println("Задание №4");
-        for (int i = 10; i >=-10; i = i -1) {
-            System.out.println(i);
+        if (login.length()>20){
+            throw new WrongLoginException("Логин не может быть длинее 20 симвалов");
         }
-        System.out.println("Задание №5");
-        for (int i = 1904; i <=2096; i = i +4) {
-            System.out.println(i+"  год является високосным");
+    }
+
+    private static void checkPassword (String pass, String confirmPass) throws WrongPasswordException {
+        if (!pass.matches(VALIDATE_PATTERN)) {
+            throw new WrongPasswordException("Пароль может содержать только латинские буквы, цыфры и знаки подчеркивания ");
         }
-        System.out.println("Задание №6");
-        for (int i = 7; i <= 98; i = i + 7) {
-            System.out.println(i);
+        if (pass.length()>20){
+            throw new WrongPasswordException("Логин не может быть длинее 20 симвалов");
         }
-        System.out.println("Задание №7");
-        for (int i = 1; i <= 1000; i = i * 2) {
-            //Ввел значение i <= 1000, интереса ради но так как программа работает так как надо оставил
-            System.out.println(i);
-        }
-        System.out.println("Задание №8");
-        int salary = 29000;
-        int total = 0;
-        for (int i = 1; i <= 12; i++) {
-            total = total + salary;
-        System.out.println("Месяц "+ i +", сумма накоплений равна "+total+" рублей");
-        }
-        System.out.println("Задание №9");
-         salary = 29000;
-         total = 0;
-        for (int i = 1; i <= 12; i++) {
-            total = total + total/100;
-            total = total + salary;
-            System.out.println("Месяц "+ i +", сумма накоплений равна "+total+" рублей");
-        }
-        System.out.println("Задание №10");
-        int number = 2;
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(number + "*" + i + "=" + (number * i));
+        if (!pass.equals(confirmPass)){
+            throw new WrongPasswordException("Пароли не совпадают");
         }
     }
 }
