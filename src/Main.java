c23f31e9cac71fba2ce8ad6ad62ba68e42babb22
Sergey Ulin <PYLIN2002@yandex.ru>
2023-12-20 12:14:21 +0300

    домашнее задание №12 переделаное

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,18 @@ import java.time.LocalDate;
 import java.util.Locale;
 
 public class Main {
+    public static void main(String[] args) {
+        Author author1 = new Author("Stephen", "King");
+        Author author2 = new Author("Chuck", "Palahniuk");
 
+        Book book1 = new Book("Оно", author1, 1986);
+        Book book2 = new Book("Бойцовский клуб", author2, 1996);
+
+        System.out.println(book1.getTitle() + " был опубликован в " + book1.getPublicationYear());
+        System.out.println(book2.getTitle() + " был опубликован в " + book2.getPublicationYear());
+
+        book1.setPublicationYear(2013);
+        System.out.println(book1.getTitle() + " был опубликован в " + book1.getPublicationYear());
+
+    }
 }
